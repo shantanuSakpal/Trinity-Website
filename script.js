f91935d0d360a33c74c00c342e43cded62d6c518
DJSCE Student <DJSCE.Student@SVKMGRP.COM>
2022-09-27 14:13:29 +0530

    home page development

---
+++ script.js
@@ -0,0 +1,21 @@
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry) => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+        else
+        {
+            entry.target.classList.remove('show');
+        }
+        
+    });
+})
+
+const hiddenEl1 = document.querySelectorAll('.hidden-content1');
+hiddenEl1.forEach((el)=>observer.observe(el))
+const hiddenEl2 = document.querySelectorAll('.hidden-content2');
+hiddenEl2.forEach((el)=>observer.observe(el))
+const hiddenEl3 = document.querySelectorAll('.hidden-content3');
+hiddenEl3.forEach((el)=>observer.observe(el))
+const hiddenEl= document.querySelectorAll('.hidden-title');
+hiddenEl.forEach((el)=>observer.observe(el))
