ceefaa64b0a8bcce550a6b02b2c6c7eae65d4798
Shantanu Sakpal <shantanuesakpal1420@gmail.com>
2022-09-29 23:36:13 +0530

    home page done

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const observer = new IntersectionObserver((entries)=>{
+const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        if(entry.isIntersecting) {
+        if (entry.isIntersecting) {
             entry.target.classList.add('show');
         }
-       
-        
+
+
     });
 })
 
 const hiddenEl1 = document.querySelectorAll('.hidden-content1');
-hiddenEl1.forEach((el)=>observer.observe(el))
+hiddenEl1.forEach((el) => observer.observe(el))
 const hiddenEl2 = document.querySelectorAll('.hidden-content2');
-hiddenEl2.forEach((el)=>observer.observe(el))
+hiddenEl2.forEach((el) => observer.observe(el))
 const hiddenEl3 = document.querySelectorAll('.hidden-content3');
-hiddenEl3.forEach((el)=>observer.observe(el))
-const hiddenEl= document.querySelectorAll('.hidden-title');
-hiddenEl.forEach((el)=>observer.observe(el))
+hiddenEl3.forEach((el) => observer.observe(el))
+const hiddenEl = document.querySelectorAll('.hidden-title');
+hiddenEl.forEach((el) => observer.observe(el))
